9f10f07120c2b111b340b33272965a5f3da45c66
sa4gu9 <34567701+sa4gu9@users.noreply.github.com>
2024-09-28 23:49:50 +0900

    창팝목록 240727까지

--- main.py
+++ main.py
@@ -11,26 +11,43 @@ if option.testMode:
 else:
     host="0.0.0.0"
 
-def getFileContent(htmlFileName):
+def getFileContent(htmlFileName,cpoplink):
     hfn = open(f"templates/{htmlFileName}.html","r",encoding="UTF-8")
-    content = hfn.read()
+    content = hfn.readlines()
     hfn.close()
-    return content
+
+    if cpoplink=="lostmedia" or cpoplink=="finding":
+        content[8]="youtube-video-link<br>"
+        del content[2:8]
+    
+    returnstr=""
+
+    for line in content:
+        returnstr+=line
+
+    return returnstr
 
 @app.route('/', methods=['GET','POST'])
 def home():
-    text=getFileContent("main")
+    
     result = None
     with open(f"list.txt","r",encoding="UTF-8") as f:
         plist = f.readlines()
         result = random.choice(plist)
         result = result.replace("\n","")
         result = result.split("...")
-
-    text=text.replace("youtube-video-link",result[0])
+    text=getFileContent("main",result[0])
+    
     text=text.replace("upload-date",result[1])
     text=text.replace("artist-name",result[2])
     text=text.replace("cpop-name",result[3])
+
+    if result[0]!="lostmedia":
+        text=text.replace("youtube-video-link",result[0])
+    text=text.replace("upload-date",result[1])
+    text=text.replace("artist-name",result[2])
+    text=text.replace("cpop-name",result[3])
+
     return text
 
 app.run(port=40109,debug=option.testMode)
