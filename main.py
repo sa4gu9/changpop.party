2dea8df69eda8e07edb0bf7fd9a99f93bf69a88c
sa4gu9 <34567701+sa4gu9@users.noreply.github.com>
2024-11-04 19:57:01 +0900

    수정, 문서버전기능 추가

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, make_response,request,redirect,url_for,jsonify
 import random
 import secret.option as option
+import os
 
 app = Flask(__name__)
 host = ""
@@ -37,7 +38,6 @@ def home():
             result = random.choice(plist)
             result = result.replace("\n","")
             result = result.split("...")
-        
     text=getFileContent("random",result[0])
 
 
@@ -59,12 +59,12 @@ def cpoplist():
     for i in text:
         if i.startswith("finding"):
             break
-        returnstr+=f'<a href="changpop?video_id={i[0:11]}"'+"</a>"+i+"<br>"
+        returnstr+=f'<a href="changpop?video_id={i[0:11]}&mode=read"'+"</a>"+i+"<br>"
 
     return render_template("main.html",text=returnstr)
 
 
-def get_document(is_kesa, video_id):
+def get_video(video_id):
     text=f"""<div class="player">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/{video_id}?autoplay=0"
     title="YouTube video player" frameborder="0"
@@ -72,23 +72,81 @@ def get_document(is_kesa, video_id):
     allowfullscreen></iframe><br>
     </div><br>"""
 
+    return text
+
+def get_document(is_kesa, video_id):
+
+    text=""
     add_text=""
     if is_kesa:
         add_text="_kesa"
     try:
-        text+=getFileContent(f"changpop{add_text}/{video_id}")
+        text+=getFileContent(f"changpop{add_text}/{video_id}/recent")
     except:
         text+="존재하지 않는 문서입니다."
 
     return text
 
 
-@app.route('/changpop', methods=['GET'])
+@app.route('/changpop', methods=['GET','POST'])
 def changpop_info():
     video_id=request.args.get("video_id")
-    text=get_document(False,video_id)
+    mode=request.args.get("mode")
+    after=""
+    if mode=="edit":
+
+        if request.method == 'POST':
+            #파일 저장
+            after=request.form['text']
+
+            
+
+            #파일의 세부 변경사항을 저장하기
+            before=get_document(False,video_id)
+            
+            if before==after:
+                pass
+            else:
+                #미디어 차단
+                if "a href" in after or "img" in after or "video" in after or "iframe" in after or "audio" in after:
+                    return redirect(f"changpop?video_id={video_id}&mode=edit")
+                count=0
+
+                #폴더의 파일 개수 확인
+                try:
+                    count=len(os.listdir(f"templates/changpop/{video_id}"))
+                except:
+                    count=1
+                    os.mkdir(f"templates/changpop/{video_id}")
+
+
+                with open(f"templates/changpop/{video_id}/V{count}.html","w",encoding="UTF-8") as f:
+                    f.write(before)
+
+                with open(f"templates/changpop/{video_id}/recent.html","w",encoding="UTF-8") as f:
+                    f.write(after)
+
+
+            #읽기모드 페이지로 url 변경
+            return redirect(f"changpop?video_id={video_id}&mode=read")
+        
+            
+        if request.method == 'GET':
+
+            #html input text로 수정할 수 있게
+            after+=f"""<form action="changpop?video_id={video_id}&mode=edit" method="post">
+            <textarea name="text" rows="10" cols="50">{get_document(False,video_id)}</textarea>
+            <input type="submit" value="수정">
+            </form>"""
+
+    elif mode=="read":
+        after=get_video(video_id)
+        after+=get_document(False,video_id)
+
+        #a href로 수정버튼 추가
+        after+=f'<a href="changpop?video_id={video_id}&mode=edit">수정</a>'
     
-    return render_template(f"main.html",text=text)
+    return render_template(f"main.html",text=after)
 
 
 @app.route('/changpop_kesa', methods=['GET'])
@@ -101,6 +159,7 @@ def changpopkesa_info():
 @app.route('/prompt', methods=['GET','POST'])
 def promptlist():
     text=getFileContent(f"prompt")
+
     return render_template(f"main.html",text=text)
 
 
@@ -114,7 +173,7 @@ def cpopkesalist():
     for i in text:
         if i.startswith("finding"):
             break
-        returnstr+=f'<a href="changpop?video_id={i[0:11]}"'+"</a>"+i+"<br>"
+        returnstr+=f'<a href="changpop?video_id={i[0:11]},mode=read"'+"</a>"+i+"<br>"
         
 
     return render_template("main.html",text=returnstr)
